3415dc41d65ea5fe3da026bddd647d913d6968a8
John Reiner <johnreiner1990@gmail.com>
2019-12-27 15:47:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,6 +66,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
+
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -81,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 end
 
